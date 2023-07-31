945b4c52cc0349cd7e895403eca434c553bb1362
Abdelrahman Mohammad <57917560+aboodmo@users.noreply.github.com>
2023-07-31 19:19:14 -0400

    - worked on the login view

---
+++ ToDoList/Views/HeaderView.swift
@@ -0,0 +1,41 @@
+//
+//  HeaderView.swift
+//  ToDoList
+//
+//  Created by Abdelrahman Mohammad on 7/30/23.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        ZStack{
+            RoundedRectangle(cornerRadius: 0)
+                .foregroundColor(.pink)
+                .rotationEffect(Angle(degrees: 15))
+            
+            VStack{
+                Text("To Do List")
+                    .font(.system(size: 50))
+                    .foregroundColor(.white)
+                    .bold()
+                
+                Text("Get Things Done")
+                    .font(.system(size: 30))
+                    .foregroundColor(.white)
+              
+            }
+            .padding(.top, 30)
+                
+        }
+        .frame(width: UIScreen.main.bounds.width * 3, height: 300)
+        .offset(y: -100)
+        
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView()
+    }
+}
--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -8,8 +8,55 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView {
+            VStack{
+                // Header
+                HeaderView()
+                
+                
+                //login form
+                Form{
+                    TextField("Email Adress", text: $email)
+                    SecureField("Password", text: $password)
+                    
+                    Button {
+                        //
+                    } label: {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(Color.blue)
+                            
+                            Text("Log In")
+                                .foregroundColor(Color.white)
+                                .bold()
+                            
+                        }
+                    }
+
+                }
+                
+                // create an account
+                VStack{
+                    Text("New around here?")
+                    NavigationLink {
+                        RegristerView()
+                    } label: {
+                        Text("Create new account")
+                    }
+
+                }
+                .padding(.bottom)
+                
+                Spacer()
+                
+            }
+        }
     }
 }
 
